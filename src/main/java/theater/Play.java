6617c2d973667487cba828c84b989929f060dc20
Charlotte Wong <charlottewong129@gmail.com>
2025-11-23 16:14:27 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,21 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+    public final String getName() {
+        return name;
+    }
+    public final String getType() {
+        return type;
     }
 }
